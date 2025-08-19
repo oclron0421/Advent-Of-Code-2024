0eb8b3cabbb63f8612ffa63c94d136561a73d5df
{Ron <ronong04@gmail.com>
2025-08-19 23:01:21 +0800

    need to edit the data typing

--- day9/day9.cpp
+++ day9/day9.cpp
@@ -25,6 +25,37 @@ long long checkSum(std::vector<DiskSpace> x, int actualSize) {
     return sum;
 }
 
+long long checkSum(std::vector<DiskSpace> x) {
+    long long sum = 0;
+    for (int i = 0; i < x.size(); i++) {
+        int digit = x[i].value; // Convert char to int
+        sum = sum + (digit * i);
+        //std::cout << "x[" << i << "] = " << digit * i << ", sum = " << sum << std::endl;
+    }
+
+    return sum;
+}
+
+int getRange(std::vector<DiskSpace>::iterator it, std::vector<DiskSpace>& fileSystem) {
+    int range = 0;
+    auto val = it->value;
+    while (it != fileSystem.begin() && it->value == val) {
+        range++;
+        it--;
+    }
+    return range;
+}
+
+bool isAllBlanks(std::vector<DiskSpace>vec) {
+    DiskSpace first = vec.front();
+    for (const auto& block : vec) {
+        if (block.fileBlock != '.' || block.value != 0) {
+            return false; // Found a non-blank space
+        }
+    }
+    return true; // All spaces are blank   
+}
+
 int main() {
     std::ifstream file("input.txt");
     std::array<int, MAX_SIZE> diskMap;
@@ -48,9 +79,9 @@ int main() {
     }
 
     //print out the diskMap
-    for (int i = 0; i < MAX_SIZE; ++i) {
-        std::cout << diskMap[i] << " ";
-    }
+    // for (int i = 0; i < MAX_SIZE; ++i) {
+    //     std::cout << diskMap[i] << " ";
+    // }
 
     bool computingBlankSpace = false;
     int count = 0;
@@ -105,7 +136,6 @@ int main() {
     }
 
     //print out the fileSystem after moving files
-    std::cout << std::endl;
     // std::cout << "File system after moving files:" << std::endl;
     // for (const auto& block : fileSystem) {
     //     if (block.fileBlock == '.')
@@ -116,7 +146,7 @@ int main() {
     //std::cout << "File size: " << actualSize << std::endl;
     //print out the fileSystem after moving files
 
-    std::cout << "Total sum of fileSystem: " << checkSum(fileSystem, actualSize) << std::endl;
+    //std::cout << "Total sum of fileSystem: " << checkSum(fileSystem, actualSize) << std::endl;
 
 
 
@@ -150,49 +180,65 @@ int main() {
 
     }
 
-    for (long long i = fileSystem.size() - 1; i > 0;i--) {
-        if (fileSystem[i].fileBlock != '.') {       //if the value is not a blank spot 
-            int range = 0;
+    // std::cout << "File system before moving files:" << std::endl;
+    // for (const auto& block : fileSystem) {
+    //     if (block.fileBlock == '.')
+    //         std::cout << block.fileBlock;
+    //     else
+    //         std::cout << block.value;
+    // }
 
-            long val = fileSystem[i].value; // Get the value of the file block
-            while (fileSystem[i - 1].value == val && i - 1 >= 0) { // Check if the previous block has the same value
-                range++;
-                i--;
-            }
 
-            bool swapped = false;
-            int startIndex = 0;
-            while (!swapped) {
-                //once i found the range, the i will find the blank spots corrosponding to the range from the start of the vector 
-                auto first_blank = std::find_if(
-                    fileSystem.begin() + startIndex,
-                    fileSystem.end(),
-                    [range](const DiskSpace& d) { return d.fileBlock == '.' && d.value == 0; }
-                );
-                int locFirstBlank = std::distance(fileSystem.begin(), first_blank); // Get the index of the first blank space
-                //find the range of the blanks 
-                startIndex = locFirstBlank + range; // Update the start index for the next searchs
-                bool spotFound = true;
-                for (int j = 0; j < range; j++) {
-                    if (fileSystem[locFirstBlank + j].fileBlock != '.') {
-                        spotFound = false;
+    std::vector<DiskSpace>::iterator backItr = fileSystem.end() - 1;
+    while (backItr != fileSystem.begin()) {
+        int range = 0;
+        bool swapped = false;
+        if (backItr->fileBlock != '.') {
+            range = getRange(backItr, fileSystem);
+
+            //iterate from the start of the vector to find the appropriate blank space
+            std::vector<DiskSpace>::iterator startItr = fileSystem.begin();
+            while (startItr != backItr - range) {
+                if (startItr->fileBlock == '.' && startItr->value == 0) {       //blank is found
+                    std::vector<DiskSpace> tempVec;
+                    for (long i = 0; i < range;i++) {
+                        tempVec.push_back(*(startItr + i)); // Store the blank space
+                    }
+                    if (isAllBlanks(tempVec)) {
+                        //commence the swap here 
+                        for (int i = 0; i < range; i++) {
+                            auto itrBlanks = startItr + i;
+                            auto itrFile = backItr - i;
+                            std::iter_swap(itrBlanks, itrFile); // Swap the blank space with the file block
+                        }
+                        //reposition the backItr 
+                        backItr -= range;
+                        swapped = true;
                         break;
                     }
                 }
-                if (spotFound) {
-                    for (int k = 0; k < range; k++) {
-                        fileSystem[locFirstBlank + k] = fileSystem[i + k];
-                        fileSystem[i + k] = { '.', 0 }; // Set the current position to blank    
-                    }
-                    swapped = true; // Mark as swapped
-                }
-                else if (!spotFound) {
-                    continue;
-                }
+                startItr++;
             }
+
+            if (!swapped) {
+                backItr -= range;
+            }
+        }
+        else {
+            backItr--;
+        }
+        std::cout << std::endl;
+        for (const auto& block : fileSystem) {
+            if (block.fileBlock == '.')
+                std::cout << block.fileBlock;
+            else
+                std::cout << block.value;
+
         }
     }
 
+    //print out the fileSystem after moving files
+    std::cout << std::endl;
     std::cout << "File system after moving files:" << std::endl;
     for (const auto& block : fileSystem) {
         if (block.fileBlock == '.')
@@ -200,5 +246,9 @@ int main() {
         else
             std::cout << block.value;
     }
+
+    std::cout << std::endl;
+    std::cout << "Total sum of fileSystem: " << checkSum(fileSystem) << std::endl;
+
     return 0;
 }
